24d14afff98076585c85c641887bb571e9df49f7
Logger <40654172+Logger@users.noreply.github.com>
2020-11-10 14:38:33 +0900

    1

---
+++ ttt.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'https://www.kbchachacha.com/public/car/detail.kbc?carSeq=20797740'
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Host': 'www.kbchachacha.com',
+    'Pragma': 'no-cache',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
+}
+response = requests.get(url, headers=headers)
+soup = bs(response.text, 'html.parser')
+Cookie = response.cookies.get('cha-cid')
+Cookie = "cha-cid=" + Cookie + ";"
