514d41861ae87dd9d52844aeabde5d7d5010d90f
Logger <40654172+Logger@users.noreply.github.com>
2020-11-17 19:37:38 +0900

    1

--- ts_copy.py
+++ ts_copy.py
@@ -30,7 +30,7 @@ def get_car_info(url, temp):
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
     }
-    time.sleep(random.randint(1, 2))
+    time.sleep(random.uniform(0.2, 1))
     response = requests.get(url, headers=headers)
     soup = bs(response.text, 'html.parser')
     soup_t = str(soup)
@@ -70,7 +70,7 @@ def get_car_info(url, temp):
         'carSeqVal': carSeq
     }
     try:
-        time.sleep(random.randint(1, 2))
+        time.sleep(random.uniform(0.2, 1))
         res_json = requests.post(
             json_url, headers=json_headers, data=json_data)
         car_dict = res_json.json()
@@ -133,7 +133,7 @@ def get_car_info(url, temp):
 
     img_src = soup.find('ul', {'class': 'bxslider'}).find('img').get('src')
     try:
-        time.sleep(random.randint(1, 2))
+        time.sleep(random.uniform(0.2, 1))
         t_date = requests.get(img_src).headers['Last-Modified'].split(' ')
         ModifiedDate = get_dateform(t_date)
     except:
@@ -215,7 +215,7 @@ def get_car_info(url, temp):
 
 
 def get_options(url, option_codes):
-    time.sleep(random.randint(1, 2))
+    time.sleep(random.uniform(0.2, 1))
     res = requests.post(
         'https://www.kbchachacha.com/public/car/option/code/list.json')
     json_datas = json.loads(res.text)
@@ -304,7 +304,7 @@ def get_history(url, temp, carHistorySeq):
         'layerId': 'layerCarHistoryInfo',
         'carHistorySeq': carHistorySeq
     }
-    time.sleep(random.randint(1, 2))
+    time.sleep(random.uniform(0.2, 1))
     response = requests.post(
         'https://www.kbchachacha.com/public/layer/car/history/info.kbc', headers=headers, data=datas)
     soup = bs(response.text, 'html.parser')
@@ -666,7 +666,7 @@ def get_checkdata(url, temp, chk_tag_url):
             'layerId': 'layerCarCheckInfo',
             'carSeq': carSeq
         }
-        time.sleep(random.randint(1, 2))
+        time.sleep(random.uniform(0.2, 1))
         res = requests.post(
             'https://www.kbchachacha.com/public/layer/car/check/info.kbc', data=data)
 
